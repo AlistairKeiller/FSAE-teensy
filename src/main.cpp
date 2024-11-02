79f14c8b57f71f6e14948d1b1848efd98f45e564
Alistair Keiller <alistair@keiller.net>
2024-11-01 18:13:46 -0700

    format

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,19 @@
 static const uint8_t POLL_RATE = 100;
 static const uint8_t READ_PINS[] = {A0, A1, A2}; // brake, shockA, shockB
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop() {
-  for (uint8_t pin : READ_PINS) {
+void loop()
+{
+  for (uint8_t pin : READ_PINS)
+  {
     int val = analogRead(pin);
     Serial.print(val);
     Serial.print(' ');
   }
   Serial.print("\n");
-  delay(1000/ POLL_RATE);
+  delay(1000 / POLL_RATE);
 }
