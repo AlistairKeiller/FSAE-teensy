6f797f14529028ab6618eaaee97be283c7e211fc
Alistair Keiller <alistair@keiller.net>
2024-11-01 18:13:10 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 
-const int POLL_RATE = 100;
+static const uint8_t POLL_RATE = 100;
+static const uint8_t READ_PINS[] = {A0, A1, A2}; // brake, shockA, shockB
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int val = analogRead(A0);
-  Serial.print("A0: ");
-  Serial.print(val);
+  for (uint8_t pin : READ_PINS) {
+    int val = analogRead(pin);
+    Serial.print(pin);
+    Serial.print(' ');
+  }
   Serial.print("\n");
   delay(1000/ POLL_RATE);
 }
