19597d88600fddc4d460796922918671f8c0143f
Alistair Keiller <alistair@keiller.net>
2024-11-01 18:17:34 -0700

    fix label

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-static const uint8_t DELAY = 10; // hz
+static const uint8_t DELAY = 10; // ms
 static const uint8_t READ_PINS[] = {A0, A1, A2}; // brake, shockA, shockB
 
 void setup()
